b7b7fc27010238fea3e8225569eb8f1bdaa49d2f
Eric Helstad <erichelstad@gmail.com>
2023-03-09 16:17:04 -0600

    Create outline for the Game class

---
+++ tic_tac_toe.rb
@@ -0,0 +1,30 @@
+class Game(playerX, playerO)
+  @game_state
+  @playerX = playerX
+  @playerO = playerO
+
+  def new_game
+
+  end
+
+  def check_for_winner
+  
+  end
+
+  def check_for_horizontal
+  
+  end
+
+  def check_for_vertical
+  
+  end
+
+  def check_for_diagonal
+  
+  end
+
+  def draw_board
+  
+  end
+end
+
